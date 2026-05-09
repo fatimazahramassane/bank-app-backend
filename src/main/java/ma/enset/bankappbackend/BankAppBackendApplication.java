d5b439ca8230273e8d4959014eac666a68b422d2
fatimazahramassane <fatimazahra.massane-etu@etu.univh2c.ma>
2026-05-09 21:18:05 +0100

    feat: add BankAccountService + RestController with DTOs & Swagger-ready endpoints

--- src/main/java/ma/enset/bankappbackend/BankAppBackendApplication.java
+++ src/main/java/ma/enset/bankappbackend/BankAppBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAppBackendApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(BankAppBackendApplication.class, args);
     }
